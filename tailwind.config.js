c85775f7ddfa1aa3a94ad6937866a8357ad8adfc
Tushar Negi <negitushar81@gmail.com>
2024-10-15 13:03:07 +0530

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,53 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      primary: "#0D6EFD",
+      secondary: "#6C757D",
+      success: "#28A745",
+      danger: "#DC3545",
+      warning: "#FFC107",
+      info: "#17A2B8",
+      light: "#F8F9FA",
+      dark: "#343A40",
+      white: "#FFFFFF",
+      black: "#000000",
+      transparent: "transparent",
+      blue: {
+        900: "#2A2F3E",
+        300: "#928A8A",
+      },
+      textMainColor: {
+        900: "#FFFFFF",
+      },
+      borderOutlineColor: {
+        900: "#a7a7a7",
+      },
+    },
+    screens: {
+      "4xl": { max: "2047px" },
+      // => @media (max-width: 2047px) { ... }
+
+      "3xl": { max: "1791px" },
+      // => @media (max-width: 1791px) { ... }
+
+      "2xl": { max: "1535px" },
+      // => @media (max-width: 1535px) { ... }
+
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: "767px" },
+      // => @media (max-width: 767px) { ... }
+
+      sm: { max: "639px" },
+      // => @media (max-width: 639px) { ... }
+    },
+    extend: {},
+  },
+  plugins: [],
+};
