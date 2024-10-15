02b13d6ed6322bb3274ed8470181d2f47de6180f
suraj singh rajput <surajthakurrs45@gmail.com>
2024-10-15 13:06:39 +0530

    heading updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1 className='bg-success'>hello</h1>
+      <h1 className='bg-success'>hello tushar bete</h1>
     </div>
   )
 }
