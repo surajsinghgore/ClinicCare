517ef86e23eae206ba8883e39b54aaa38c385c4a
Tushar Negi <negitushar81@gmail.com>
2024-10-15 13:31:12 +0530

    file setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,6 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1 className='bg-success'>hello ,
-      sfdfsdfs
-      ,fsdfsd
-      
-      
-      ,fsdfsd
-      
-      fsdfsdtushar bete</h1>
-      <h1 className='font-semibold'>ClinicCare</h1>
-      <h2>suraj singh</h2>
-      <h1 className='bg-success'>hello tushar bete</h1>
     </div>
   )
 }
